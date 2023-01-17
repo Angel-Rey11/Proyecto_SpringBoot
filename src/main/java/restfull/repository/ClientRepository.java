5dbdebe344a2c7bf1a17ea59b8251b49ed8bff2b
Angel-Rey11 <91887811+Angel-Rey11@users.noreply.github.com>
2023-01-17 11:05:36 +0100

    repos

--- src/main/java/restfull/repository/ClientRepository.java
+++ src/main/java/restfull/repository/ClientRepository.java
@@ -1,5 +1,11 @@
 package restfull.repository;
 
-public class ClientRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import restfull.model.Client;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long>{
+	
 }
