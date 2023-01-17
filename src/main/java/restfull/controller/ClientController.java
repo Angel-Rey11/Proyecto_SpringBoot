de7c772619873704f1a64020674bbaadc3c314fc
Angel-Rey11 <91887811+Angel-Rey11@users.noreply.github.com>
2023-01-17 19:58:33 +0100

    no esta mal

--- src/main/java/restfull/controller/ClientController.java
+++ src/main/java/restfull/controller/ClientController.java
@@ -1,5 +1,42 @@
 package restfull.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import restfull.model.Client;
+import restfull.service.ClientService;
+
+@RestController
+@RequestMapping("/clients")
 public class ClientController {
+	
+	@Autowired
+	ClientService service;
+	
+	@GetMapping
+	public ResponseEntity<List<Client>> getAllCars() {
+		List<Client> result = service.getAllClient();
+		return new ResponseEntity<List<Client>>(result, new HttpHeaders(), HttpStatus.OK);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Client> getItemById(@PathVariable("id") Long id) {
+		Client c = service.getClientById(id);
+		return new ResponseEntity<Client>(c, new HttpHeaders(), HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/{id}")
+	public HttpStatus deleteClient(@PathVariable("id") Long id) {
+		service.removeClient(id);
+		return HttpStatus.OK;
+	}
 
 }
--- src/main/java/restfull/service/CarService.java
+++ src/main/java/restfull/service/CarService.java
@@ -1,5 +1,44 @@
 package restfull.service;
 
-public class CarService {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import restfull.model.Car;
+import restfull.repository.CarRepository;
+
+@Service
+public class CarService {
+	
+	@Autowired
+	CarRepository repository;
+	
+	public Car addCar(Car c) {
+		try {
+			Optional<Car> coche = repository.findById(c.getId());
+			if (!coche.isPresent()) {
+				c = repository.save(c);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return c;
+	}
+	
+	public List<Car> getAllCars() {
+		return repository.findAll();
+	}
+	
+	public Car getCarById(Long id) {
+		Car car = new Car();
+		try {
+			Optional<Car> c = repository.findById(id);
+			car = c.get();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return car;
+	}
 }
